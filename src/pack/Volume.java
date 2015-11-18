3796004af7ea15acf43254d1bbaa3d3cdf061593
Shamil911 <Dilmuhametov74@bk.ru>
2015-11-18 14:50:24 +0500

    Update Volume.java

--- src/pack/Volume.java
+++ src/pack/Volume.java
@@ -48,4 +48,54 @@ public class Volume {
 			/* объем рассчитывается по формуле v=out_a*out_h*out_l. умножить на 1000 для перевода м^3 в литры.
 			 * делим на 100 каждое число для перевода см в метры  */
 			res_out_v=res_out_a/100*res_out_h/100*res_out_l/100*1000;
-			
+						
+		} else { //пользователь ввел внешние размеры
+			/* внутренние размеры рассчитываются как внешние размеры минус толщина двух стенок */
+			res_in_a=d_in_a-2*d_in_t;
+			res_in_h=d_in_h-2*d_in_t;
+			res_in_l=d_in_l-2*d_in_t;
+			res_in_t=d_in_t;
+			/* объем рассчитывается по формуле v=in_a*in_h*in_l.умножить на 1000 для перевода м^3 в литры.
+			 * делим на 100 каждое число для перевода см в метры  */
+			res_in_v=res_in_a/100*res_in_h/100*res_in_l/100*1000;
+			res_out_a=d_in_a;
+			res_out_h=d_in_h;
+			res_out_l=d_in_l;
+			/* объем рассчитывается по формуле v=out_a*out_h*out_l. умножить на 1000 для перевода м^3 в литры.
+			 * делим на 100 каждое число для перевода см в метры  */
+			res_out_v=res_out_a/100*res_out_h/100*res_out_l/100*1000;
+		}
+		
+		//форматированный вывод
+	    String in_a_str = String.format("%.2f", res_in_a);
+	    String in_h_str = String.format("%.2f", res_in_h);
+	    String in_l_str = String.format("%.2f", res_in_l);
+	    String in_t_str = String.format("%.2f", res_in_t);
+	    String in_v_str = String.format("%.2f", res_in_v);
+	    String out_a_str = String.format("%.2f", res_out_a);
+	    String out_h_str = String.format("%.2f", res_out_h);
+	    String out_l_str = String.format("%.2f", res_out_l);
+	    String out_v_str = String.format("%.2f", res_out_v);
+	    /* String - указываем что переменная будет иметь строковый тип,
+	     * String_sred_100km_1 - название новой переменной,
+	     * String.format - преобразование переменной в строкой тип,
+	     	%.2f - указываем что переменная будет отображать только 2 цифры после запятой,
+	     	double_sred_100km - переменная типа double, которую надо преобразовать в строковый тип
+	     	 */
+		
+	    Result.in_a_label.setText(in_a_str);
+	    Result.in_h_label.setText(in_h_str);
+	    Result.in_l_label.setText(in_l_str);
+	    Result.in_t_label.setText(in_t_str);
+	    Result.in_v_label.setText(in_v_str);
+	    Result.out_a_label.setText(out_a_str);
+	    Result.out_h_label.setText(out_h_str);
+	    Result.out_l_label.setText(out_l_str);
+	    Result.out_v_label.setText(out_v_str);
+ 
+	    /* Записываем строковые переменные в текстовые поля Name.., которые расположены в файле Result */
+		
+	   
+	}	
+}
+
